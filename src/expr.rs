69f31ac81fc65d6bb0951a1a5c64885f1bd28e75
Jakub Antonín Štigler <xstigl00@stud.fit.vut.cz>
2026-05-17 13:22:43 +0200

    Use less brackets when printing expressions.

--- src/expr.rs
+++ src/expr.rs
@@ -12,23 +12,47 @@ pub enum Expr {
     Increment,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum LastType {
+    Root,
+    Lambda,
+    ApplyLeft,
+    ApplyRight,
+}
+
 impl Expr {
     pub fn to_string(&self, itab: &ITab, res: &mut String) {
+        self.to_string_inner(itab, res, LastType::Root);
+    }
+
+    fn to_string_inner(&self, itab: &ITab, res: &mut String, last: LastType) {
         match self {
             Expr::Ident(id) => *res += &itab.name_of(*id),
             Expr::Apply(l, r) => {
-                res.push('(');
-                l.to_string(itab, res);
+                let brackets = last == LastType::ApplyRight;
+                if brackets {
+                    res.push('(');
+                }
+                l.to_string_inner(itab, res, LastType::ApplyLeft);
                 res.push(' ');
-                r.to_string(itab, res);
-                res.push(')');
+                r.to_string_inner(itab, res, LastType::ApplyRight);
+                if brackets {
+                    res.push(')');
+                }
             }
             Expr::Lambda(id, expr) => {
-                *res += "(\\";
+                let brackets =
+                    matches!(last, LastType::ApplyLeft | LastType::ApplyRight);
+                if brackets {
+                    res.push('(');
+                }
+                res.push('\\');
                 *res += &itab.name_of(*id);
                 res.push('.');
-                expr.to_string(itab, res);
-                res.push(')');
+                expr.to_string_inner(itab, res, LastType::Lambda);
+                if brackets {
+                    res.push(')');
+                }
             }
             Expr::Counter(cnt) => _ = write!(res, ":{cnt}:"),
             Expr::Increment => *res += "$increment",
